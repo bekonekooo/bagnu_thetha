1c09b3829bb86ceb26604fa4dd00bee369cf8ed9
Berkay <berkayyaydin@gmail.com>
2026-05-28 14:01:09 +0300

    v27 ai

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,11 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   audioplayers_windows
+  connectivity_plus
   file_selector_windows
+  flutter_webrtc
+  livekit_client
+  permission_handler_windows
   url_launcher_windows
 )
 
